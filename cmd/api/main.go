244c95f1f5873caa162b7ddf0a270e2b23d16aa9
Josh Duffney <jduffney@microsoft.com>
2022-12-30 09:40:12 -0600

    create healthcheckHandler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environemnt (dev|stage|prod")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	addr := fmt.Sprintf(":%d", cfg.port)
+
+	http.HandleFunc("/v1/healthcheck", healthcheckHandler)
+
+	logger.Printf("starting %s server on %s", cfg.env, addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
+func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintln(w, "environment: development")
+	fmt.Fprintf(w, "version: %s\n", version)
 }
