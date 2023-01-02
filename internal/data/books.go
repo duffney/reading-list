4ec52755f2a92716094eecd286bb95b8caf98a3e
Josh Duffney <jduffney@microsoft.com>
2023-01-02 06:56:20 -0600

    Use struct tag directive to convert int to string

--- internal/data/books.go
+++ internal/data/books.go
@@ -8,9 +8,9 @@ type Book struct {
 	ID            int64     `json:"id"`
 	CreatedAt     time.Time `json:"-"`
 	Title         string    `json:"title"`
-	Subtitle      string    `json:"year"`
+	Subtitle      string    `json:"subtitle"`
 	PublishedDate time.Time `json:"published_date,omitempty"` //year, month, day := fromDate.Date()
-	Pages         int       `json:"pages,omitempty"`
+	Pages         int       `json:"pages,omitempty,string"`   //changed int to string in JSON
 	Genres        []string  `json:"genres,omitempty"`
 	Rating        int       `json:"rating,omitempty"`
 	Version       int32     `json:"version"`
