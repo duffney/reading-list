d844c1494aecc179dcf9c160945cb1f96bab43b0
Josh Duffney <jduffney@microsoft.com>
2023-01-02 06:45:04 -0600

    Add JSON struct keys

--- internal/data/books.go
+++ internal/data/books.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Book struct {
-	ID            int64
-	CreatedAt     time.Time
-	Title         string
-	Subtitle      string
-	PublishedDate time.Time //year, month, day := fromDate.Date()
-	Pages         int
-	Genres        []string
-	Rating        int
-	Version       int32
+	ID            int64     `json:"id"`
+	CreatedAt     time.Time `json:"created_at"`
+	Title         string    `json:"title"`
+	Subtitle      string    `json:"year"`
+	PublishedDate time.Time `json:"published_date"` //year, month, day := fromDate.Date()
+	Pages         int       `json:"pages"`
+	Genres        []string  `json:"genres"`
+	Rating        int       `json:"rating"`
+	Version       int32     `json:"version"`
 }
 
 // TODO Create a custom type for PublishedDate like Runtime in Let's Go Further
